00a967931cd14cbab6c2695d9a52e29400e15f70
mohsen220 <68242902+mohsen220@users.noreply.github.com>
2024-06-01 12:44:02 -0400

    go

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,25 @@ module.exports = {
   apps: [
     {
       name: "my-app",
-      script: "./app.js", // Path to your app's main file
+      script: "npm",
+      args: "run dev",
       watch: true,
       env: {
         NODE_ENV: "development",
+        PATH:
+          process.env.PATH +
+          ":/home/ec2-user/project/Vention/node_modules/.bin",
       },
       env_production: {
         NODE_ENV: "production",
+        PATH:
+          process.env.PATH +
+          ":/home/ec2-user/project/Vention/node_modules/.bin",
       },
+      out_file: "/home/ec2-user/project/Vention/logs/my-app-out.log",
+      error_file: "/home/ec2-user/project/Vention/logs/my-app-error.log",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
     },
   ],
 };
+  
