c55bfd2b6e6382a9fff7946046ff0db1e27b870b
mohsen220 <68242902+mohsen220@users.noreply.github.com>
2024-06-01 12:25:52 -0400

    ss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "my-app",
-      script: "./node_modules/.bin/webpack",
-      args: "serve",
+      name: "app",
+      script: "./index.js",
       watch: true,
       env: {
         NODE_ENV: "development",
-        PATH:
-          process.env.PATH +
-          ":/home/ec2-user/project/Vention/node_modules/.bin",
       },
       env_production: {
         NODE_ENV: "production",
-        PATH:
-          process.env.PATH +
-          ":/home/ec2-user/project/Vention/node_modules/.bin",
       },
     },
   ],
