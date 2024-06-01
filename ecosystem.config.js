ef37167a73412f2d19956a0db8ac555e8d89cb36
mohsen220 <68242902+mohsen220@users.noreply.github.com>
2024-06-01 12:17:53 -0400

    test 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,15 @@ module.exports = {
       watch: true,
       env: {
         NODE_ENV: "development",
+        PATH:
+          process.env.PATH +
+          ":/home/ec2-user/project/Vention/node_modules/.bin",
       },
       env_production: {
         NODE_ENV: "production",
+        PATH:
+          process.env.PATH +
+          ":/home/ec2-user/project/Vention/node_modules/.bin",
       },
     },
   ],
