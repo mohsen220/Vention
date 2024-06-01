dcbaf3e51ed59ea9d1b70ec1b7837d1f833e80c3
mohsen220 <68242902+mohsen220@users.noreply.github.com>
2024-06-01 10:44:55 -0400

    commit

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-app",
+      script: "./src/index.js",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
