3534a3b11478ddc4d99b0bd268cd3704167749d1
mohsen220 <68242902+mohsen220@users.noreply.github.com>
2024-06-02 17:27:40 -0400

    commit

--- cypress/integration/shape_management.js
+++ cypress/integration/shape_management.js
@@ -1,5 +1,7 @@
-function validation(window,color,position,type){
-  cy.window().should(window => {
+import "@percy/cypress";
+
+function validation(window, color, position, type) {
+  cy.window().should((window) => {
     console.log(window.shapes);
     expect(shape).to.deep.include({
       options: {
@@ -7,302 +9,304 @@ function validation(window,color,position,type){
         position: {
           x: position.x,
           y: position.y,
-          z: position.z
-        }
+          z: position.z,
+        },
       },
-      type: type
+      type: type,
     });
-});
+  });
 }
 
-describe('Functionality & Validation Testing', () => {
-    beforeEach(() => {
-      // Since the app is served on localhost:8080ß
-      cy.visit('http://localhost:8080');
-    });
-  
-    //Positive Test Cases
-    it('Adding & Deleting 1 Sphere Object ', () => {
-      // Fill the position and color inputs
-      cy.get('#pos_x').clear().type('2');
-      cy.get('#pos_y').clear().type('2');
-      cy.get('#pos_z').clear().type('2');
-      cy.get('#color').type("#000000");
-  
-      // Click the add sphere button
-      cy.get('#add_sphere').click();
+describe("Functionality & Validation Testing", () => {
+  beforeEach(() => {
+    // Since the app is served on localhost:8080ß
+    cy.visit("http://localhost:8080");
+  });
 
-      // Validation 
-      cy.window().should(win => {
-        console.log(win.shapes);
-        expect(win.shapes[0]).to.deep.include({
-          options: {
-            color: '#000000',
-            position: {
-              x: '2',
-              y: '2',
-              z: '2'
-            }
+  //Positive Test Cases
+  it("Adding & Deleting 1 Sphere Object ", () => {
+    // Fill the position and color inputs
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
+
+    // Click the add sphere button
+    cy.get("#add_sphere").click();
+
+    // Validation
+    cy.window().should((win) => {
+      console.log(win.shapes);
+      expect(win.shapes[0]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "2",
+            y: "2",
+            z: "2",
           },
-          type: 'sphere'
-        });
+        },
+        type: "sphere",
       });
+    });
 
-      //Validation using function 
-      //validation(window,'#000000',{x:'2',y:'2',z:'2'},'sphere');
+    //Validation using function
+    //validation(window,'#000000',{x:'2',y:'2',z:'2'},'sphere');
 
     cy.wait(5000);
 
     // Click the remove last button to delete the sphere
-    cy.get('#remove_last').click();
+    cy.get("#remove_last").click();
+  });
+
+  it("Adding & Deleting 1 Cube Object", () => {
+    // Fill the position and color inputs
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
+
+    // Click the add cube button
+    cy.get("#add_cube").click();
+
+    // Validation
+    cy.window().should((win) => {
+      console.log(win.shapes);
+      expect(win.shapes[0]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "2",
+            y: "2",
+            z: "2",
+          },
+        },
+        type: "cube",
+      });
+    });
+    cy.wait(5000);
+
+    // Click the remove last button to delete the cube
+    cy.get("#remove_last").click();
+  });
+
+  it("Creating Multiple Shapes and Removing All - Different Positions", () => {
+    // Assuming there's already shapes present
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
+    cy.get("#add_sphere").click();
+    cy.wait(5000);
+
+    cy.get("#pos_x").clear().type("3");
+    cy.get("#pos_y").clear().type("3");
+    cy.get("#pos_z").clear().type("3");
+    cy.get("#color").type("#832020");
+    cy.get("#add_cube").click();
+    cy.wait(5000);
+
+    cy.get("#pos_x").clear().type("4");
+    cy.get("#pos_y").clear().type("4");
+    cy.get("#pos_z").clear().type("4");
+    cy.get("#color").type("#000000");
+    cy.get("#add_sphere").click();
+    cy.wait(5000);
+
+    cy.get("#pos_x").clear().type("5");
+    cy.get("#pos_y").clear().type("5");
+    cy.get("#pos_z").clear().type("5");
+    cy.get("#color").type("$000000");
+    cy.get("#add_cube").click();
+    cy.wait(5000);
+
+    //Validation
+    cy.window().should((win) => {
+      console.log(win.shapes);
+      expect(win.shapes).to.have.length(4);
     });
 
-    it('Adding & Deleting 1 Cube Object', () => {
-        // Fill the position and color inputs
-        cy.get('#pos_x').clear().type('2');
-        cy.get('#pos_y').clear().type('2');
-        cy.get('#pos_z').clear().type('2');
-        cy.get('#color').type("#000000");
-    
-        // Click the add cube button
-        cy.get('#add_cube').click();
-
-        // Validation
-        cy.window().should(win => {
-          console.log(win.shapes);
-          expect(win.shapes[0]).to.deep.include({
-            options: {
-              color: '#000000',
-              position: {
-                x: '2',
-                y: '2',
-                z: '2'
-              }
-            },
-            type: 'cube'
-          });
-      });
-      cy.wait(5000);
-
-      // Click the remove last button to delete the cube
-      cy.get('#remove_last').click();
-      });
-  
-    
-    it('Creating Multiple Shapes and Removing All - Different Positions', () => {
-        // Assuming there's already shapes present
-        cy.get('#pos_x').clear().type('2');
-        cy.get('#pos_y').clear().type('2');
-        cy.get('#pos_z').clear().type('2');
-        cy.get('#color').type("#000000");
-        cy.get('#add_sphere').click();
-        cy.wait(5000);
-
-        cy.get('#pos_x').clear().type('3');
-        cy.get('#pos_y').clear().type('3');
-        cy.get('#pos_z').clear().type('3');
-        cy.get('#color').type("#832020");
-        cy.get('#add_cube').click();
-        cy.wait(5000);
-
-        cy.get('#pos_x').clear().type('4');
-        cy.get('#pos_y').clear().type('4');
-        cy.get('#pos_z').clear().type('4');
-        cy.get('#color').type("#000000");
-        cy.get('#add_sphere').click();
-        cy.wait(5000);
-
-        cy.get('#pos_x').clear().type('5');
-        cy.get('#pos_y').clear().type('5');
-        cy.get('#pos_z').clear().type('5');
-        cy.get('#color').type("$000000"); 
-        cy.get('#add_cube').click();
-        cy.wait(5000);
-
-        //Validation
-        cy.window().should(win => {
-          console.log(win.shapes);
-          expect(win.shapes).to.have.length(4);
-        });
-
-        //Validate each element in the array
-        cy.window().should(win => {
-          console.log(win.shapes);
-          expect(win.shapes[0]).to.deep.include({
-            options: {
-              color: '#000000',
-              position: {
-                x: '2',
-                y: '2',
-                z: '2'
-              }
-            },
-            type: 'sphere'
-          });
-          expect(win.shapes[1]).to.deep.include({
-            options: {
-              color: '#000000',
-              position: {
-                x: '3',
-                y: '3',
-                z: '3'
-              }
-            },
-            type: 'cube'
-          });
-          expect(win.shapes[2]).to.deep.include({
-            options: {
-              color: '#000000',
-              position: {
-                x: '4',
-                y: '4',
-                z: '4'
-              }
-            },
-            type: 'sphere'
-          });
-          expect(win.shapes[3]).to.deep.include({
-            options: {
-              color: '#000000',
-              position: {
-                x: '5',
-                y: '5',
-                z: '5'
-              }
-            },
-            type: 'cube'
-          });
-        })
-      
-        cy.get('#remove_all').click();
-        cy.wait(5000);
-
-        // Validation
-        cy.window().should(win => {
-          console.log(win.shapes);
-          expect(win.shapes).to.be.empty;
-        });
-
-    });
-
-    it('Creating Multiple Shapes and Removing All - Same Position', () => {
-      cy.get('#pos_x').clear().type('2');
-      cy.get('#pos_y').clear().type('2');
-      cy.get('#pos_z').clear().type('2');
-      cy.get('#color').type("#000000");
-      cy.get('#add_sphere').click();
-      cy.wait(5000);
-
-      cy.get('#pos_x').clear().type('2');
-      cy.get('#pos_y').clear().type('2');
-      cy.get('#pos_z').clear().type('2');
-      cy.get('#color').type("#000000");
-      cy.get('#add_cube').click();
-      cy.wait(5000);
-
-      cy.get('#pos_x').clear().type('2');
-      cy.get('#pos_y').clear().type('2');
-      cy.get('#pos_z').clear().type('2');
-      cy.get('#color').type("#000000");
-      cy.get('#add_sphere').click();
-      cy.wait(5000);
-
-      cy.get('#pos_x').clear().type('2');
-      cy.get('#pos_y').clear().type('2');
-      cy.get('#pos_z').clear().type('2');
-      cy.get('#color').type("#000000");
-      cy.get('#add_cube').click();
-      cy.wait(5000);
-
-      //Validation
-      cy.window().should(win => {
-        console.log("Im at the validation step" + win.shapes);
-        expect(win.shapes).to.have.length(4);
-      });
-
-      //Validate each element in the array
-      cy.window().should(win => {
-        console.log(win.shapes);
-        expect(win.shapes[0]).to.deep.include({
-          options: {
-            color: '#000000',
-            position: {
-              x: '2',
-              y: '2',
-              z: '2'
-            }
+    //Validate each element in the array
+    cy.window().should((win) => {
+      console.log(win.shapes);
+      expect(win.shapes[0]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "2",
+            y: "2",
+            z: "2",
           },
-          type: 'sphere'
-        });
-        expect(win.shapes[1]).to.deep.include({
-          options: {
-            color: '#000000',
-            position: {
-              x: '2',
-              y: '2',
-              z: '2'
-            }
-          },
-          type: 'cube'
-        });
-        expect(win.shapes[2]).to.deep.include({
-          options: {
-            color: '#000000',
-            position: {
-              x: '2',
-              y: '2',
-              z: '2'
-            }
-          },
-          type: 'sphere'
-        });
-        expect(win.shapes[3]).to.deep.include({
-          options: {
-            color: '#000000',
-            position: {
-              x: '2',
-              y: '2',
-              z: '2'
-            }
-          },
-          type: 'cube'
-        });
+        },
+        type: "sphere",
       });
-
-      cy.get('#remove_all').click();
-      cy.wait(5000);
-
-      // Validation
-      cy.window().should(win => {
-        console.log(win.shapes);
-        expect(win.shapes).to.be.empty;
+      expect(win.shapes[1]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "3",
+            y: "3",
+            z: "3",
+          },
+        },
+        type: "cube",
+      });
+      expect(win.shapes[2]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "4",
+            y: "4",
+            z: "4",
+          },
+        },
+        type: "sphere",
+      });
+      expect(win.shapes[3]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "5",
+            y: "5",
+            z: "5",
+          },
+        },
+        type: "cube",
       });
     });
 
-    it('Handles window resize correctly', () => {
-      // Trigger a window resize
-      cy.viewport(800, 600); // Change viewport size
-      cy.wait(5000);
+    cy.get("#remove_all").click();
+    cy.wait(5000);
+
+    // Validation
+    cy.window().should((win) => {
+      console.log(win.shapes);
+      expect(win.shapes).to.be.empty;
     });
-  })
+  });
 
+  it("Creating Multiple Shapes and Removing All - Same Position", () => {
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
+    cy.get("#add_sphere").click();
+    cy.wait(5000);
 
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
+    cy.get("#add_cube").click();
+    cy.wait(5000);
 
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
+    cy.get("#add_sphere").click();
+    cy.wait(5000);
 
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
+    cy.get("#add_cube").click();
+    cy.wait(5000);
 
+    //Validation
+    cy.window().should((win) => {
+      console.log("Im at the validation step" + win.shapes);
+      expect(win.shapes).to.have.length(4);
+    });
 
+    //Validate each element in the array
+    cy.window().should((win) => {
+      console.log(win.shapes);
+      expect(win.shapes[0]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "2",
+            y: "2",
+            z: "2",
+          },
+        },
+        type: "sphere",
+      });
+      expect(win.shapes[1]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "2",
+            y: "2",
+            z: "2",
+          },
+        },
+        type: "cube",
+      });
+      expect(win.shapes[2]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "2",
+            y: "2",
+            z: "2",
+          },
+        },
+        type: "sphere",
+      });
+      expect(win.shapes[3]).to.deep.include({
+        options: {
+          color: "#000000",
+          position: {
+            x: "2",
+            y: "2",
+            z: "2",
+          },
+        },
+        type: "cube",
+      });
+    });
 
+    cy.get("#remove_all").click();
+    cy.wait(5000);
 
+    // Validation
+    cy.window().should((win) => {
+      console.log(win.shapes);
+      expect(win.shapes).to.be.empty;
+    });
+  });
 
+  it("Handles window resize correctly", () => {
+    // Trigger a window resize
+    cy.viewport(800, 600); // Change viewport size
+    cy.wait(5000);
+  });
+});
 
+describe("UI Testing", () => {
+  beforeEach(() => {
+    // Since the app is served on localhost:8080
+    cy.visit("http://localhost:8080");
+  });
 
+  it("Should render the Shape Correctly", () => {
+    // Fill the position and color inputs
+    cy.get("#pos_x").clear().type("2");
+    cy.get("#pos_y").clear().type("2");
+    cy.get("#pos_z").clear().type("2");
+    cy.get("#color").type("#000000");
 
+    // Click the add sphere button
+    cy.get("#add_sphere").click();
+    cy.wait(5000);
 
-
-
-
-
-
+    // Take a snanpshot
+    cy.percySnapshot("Sphere Shape");
+  });
+});
 
 /* describe('Error Handling Testing', () => {
   beforeEach(() => {
@@ -369,8 +373,3 @@ describe('Functionality & Validation Testing', () => {
   })
 });
 */
-
-
-
-
-  
